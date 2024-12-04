f756d7f4fd9e2b266d9f2499c3e84323372cab0c
Clare Liguori <liguori@amazon.com>
2024-12-03 16:07:20 -0800

    Prettier

--- infra/service.ts
+++ infra/service.ts
@@ -34,30 +34,28 @@ class BlueskyPdsInfraStack extends Stack {
         {
           name: 'Public',
           subnetType: ec2.SubnetType.PUBLIC,
-        }
+        },
       ],
     });
     const cluster = new ecs.Cluster(this, 'Cluster', {
       clusterName: props.domainName.replace(/\./g, '-'),
       vpc,
     });
-    const domainZone = route53.HostedZone.fromLookup(this, 'Zone', { domainName: props.domainZone });
+    const domainZone = route53.HostedZone.fromLookup(this, 'Zone', {
+      domainName: props.domainZone,
+    });
 
     // Resources for the PDS application
-    const adminPassword = new secretsmanager.Secret(this, 'AdminPassword',
-      {
-        generateSecretString: {
-          passwordLength: 16,
-        },
-      }
-    );
-    const jwtSecret = new secretsmanager.Secret(this, 'JwtSecret',
-      {
-        generateSecretString: {
-          passwordLength: 16,
-        },
-      }
-    );
+    const adminPassword = new secretsmanager.Secret(this, 'AdminPassword', {
+      generateSecretString: {
+        passwordLength: 16,
+      },
+    });
+    const jwtSecret = new secretsmanager.Secret(this, 'JwtSecret', {
+      generateSecretString: {
+        passwordLength: 16,
+      },
+    });
     const rotationKey = new kms.Key(this, 'RotationKey', {
       keySpec: kms.KeySpec.ECC_SECG_P256K1,
       keyUsage: kms.KeyUsage.SIGN_VERIFY,
@@ -66,24 +64,24 @@ class BlueskyPdsInfraStack extends Stack {
 
     // TODO mechanism for rotating the password, JWT, and rotation key
 
-    const blobBucket = new s3.Bucket(this, "BlobStorage", {
-        removalPolicy: RemovalPolicy.DESTROY,
-        autoDeleteObjects: true,
-        blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
-      },
-    );
+    const blobBucket = new s3.Bucket(this, 'BlobStorage', {
+      removalPolicy: RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+    });
 
-    const dataBackupBucket = new s3.Bucket(this, "DataBackupStorage", {
-        removalPolicy: RemovalPolicy.DESTROY,
-        autoDeleteObjects: true,
-        blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
-      },
-    );
+    const dataBackupBucket = new s3.Bucket(this, 'DataBackupStorage', {
+      removalPolicy: RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+    });
 
     // ECR pull-through cache for the PDS image on GHCR
     const githubSecret = secretsmanager.Secret.fromSecretNameV2(
-      this, 'GitHubToken', 'ecr-pullthroughcache/bluesky-pds-image-github-token',
-    )
+      this,
+      'GitHubToken',
+      'ecr-pullthroughcache/bluesky-pds-image-github-token'
+    );
     new ecr.CfnPullThroughCacheRule(this, 'ContainerImagePullThroughCache', {
       credentialArn: githubSecret.secretArn,
       ecrRepositoryPrefix: 'github-bluesky',
@@ -97,105 +95,107 @@ class BlueskyPdsInfraStack extends Stack {
     const image = ecs.ContainerImage.fromEcrRepository(cacheRepo, '0.4');
 
     // Fargate service + load balancer to run PDS container image
-    const service = new patterns.ApplicationLoadBalancedFargateService(this, 'Service', {
-      cluster,
-      serviceName: props.domainName.replace(/\./g, '-'),
-      desiredCount: 1,
-      domainName: props.domainName,
-      domainZone,
-      protocol: ApplicationProtocol.HTTPS,
-      redirectHTTP: true,
-      assignPublicIp: true,
-      propagateTags: ecs.PropagatedTagSource.SERVICE,
-      // PDS server configuration
-      taskImageOptions: {
-        containerName: 'pds',
-        image,
-        containerPort: 3000,
-        logDriver: ecs.LogDriver.awsLogs({
-          streamPrefix: 'PDSService',
-          logGroup: new logs.LogGroup(this, 'ServiceLogGroup', {
-            retention: logs.RetentionDays.ONE_MONTH,
-            removalPolicy: RemovalPolicy.DESTROY,
+    const service = new patterns.ApplicationLoadBalancedFargateService(
+      this,
+      'Service',
+      {
+        cluster,
+        serviceName: props.domainName.replace(/\./g, '-'),
+        desiredCount: 1,
+        domainName: props.domainName,
+        domainZone,
+        protocol: ApplicationProtocol.HTTPS,
+        redirectHTTP: true,
+        assignPublicIp: true,
+        propagateTags: ecs.PropagatedTagSource.SERVICE,
+        // PDS server configuration
+        taskImageOptions: {
+          containerName: 'pds',
+          image,
+          containerPort: 3000,
+          logDriver: ecs.LogDriver.awsLogs({
+            streamPrefix: 'PDSService',
+            logGroup: new logs.LogGroup(this, 'ServiceLogGroup', {
+              retention: logs.RetentionDays.ONE_MONTH,
+              removalPolicy: RemovalPolicy.DESTROY,
+            }),
           }),
-        }),
-        environment: {
-          // TODO OAuth config
-          PDS_HOSTNAME: props.domainName,
-          PDS_PORT: "3000",
-          PDS_DATA_DIRECTORY: '/pds',
-          PDS_PLC_ROTATION_KEY_KMS_KEY_ID: rotationKey.keyId,
-          AWS_REGION: this.region,
-          AWS_DEFAULT_REGION: this.region,
-          PDS_BLOBSTORE_S3_BUCKET: blobBucket.bucketName,
-          PDS_BLOBSTORE_S3_REGION: this.region,
-          PDS_BLOBSTORE_DISK_LOCATION: '',
-          PDS_BLOB_UPLOAD_LIMIT: '52428800',
-          PDS_DID_PLC_URL: 'https://plc.directory',
-          PDS_BSKY_APP_VIEW_URL: 'https://api.bsky.app',
-          PDS_BSKY_APP_VIEW_DID: 'did:web:api.bsky.app',
-          PDS_REPORT_SERVICE_URL: 'https://mod.bsky.app',
-          PDS_REPORT_SERVICE_DID: 'did:plc:ar7c4by46qjdydhdevvrndac',
-          PDS_CRAWLERS: 'https://bsky.network',
-          PDS_SERVICE_HANDLE_DOMAINS: '.' + props.rootDomain,
-          LOG_ENABLED: 'true',
+          environment: {
+            // TODO OAuth config
+            PDS_HOSTNAME: props.domainName,
+            PDS_PORT: '3000',
+            PDS_DATA_DIRECTORY: '/pds',
+            PDS_PLC_ROTATION_KEY_KMS_KEY_ID: rotationKey.keyId,
+            AWS_REGION: this.region,
+            AWS_DEFAULT_REGION: this.region,
+            PDS_BLOBSTORE_S3_BUCKET: blobBucket.bucketName,
+            PDS_BLOBSTORE_S3_REGION: this.region,
+            PDS_BLOBSTORE_DISK_LOCATION: '',
+            PDS_BLOB_UPLOAD_LIMIT: '52428800',
+            PDS_DID_PLC_URL: 'https://plc.directory',
+            PDS_BSKY_APP_VIEW_URL: 'https://api.bsky.app',
+            PDS_BSKY_APP_VIEW_DID: 'did:web:api.bsky.app',
+            PDS_REPORT_SERVICE_URL: 'https://mod.bsky.app',
+            PDS_REPORT_SERVICE_DID: 'did:plc:ar7c4by46qjdydhdevvrndac',
+            PDS_CRAWLERS: 'https://bsky.network',
+            PDS_SERVICE_HANDLE_DOMAINS: '.' + props.rootDomain,
+            LOG_ENABLED: 'true',
+          },
+          secrets: {
+            PDS_ADMIN_PASSWORD: ecs.Secret.fromSecretsManager(adminPassword),
+            PDS_JWT_SECRET: ecs.Secret.fromSecretsManager(jwtSecret),
+          },
         },
-        secrets: {
-          PDS_ADMIN_PASSWORD: ecs.Secret.fromSecretsManager(adminPassword),
-          PDS_JWT_SECRET: ecs.Secret.fromSecretsManager(jwtSecret),
+        healthCheck: {
+          command: [
+            'CMD-SHELL',
+            "node -e 'fetch(`http://localhost:3000/xrpc/_health`).then(()=>process.exitCode = 0).catch(()=>process.exitCode = 1)'",
+          ],
         },
-      },
-      healthCheck: {
-        command: ['CMD-SHELL', "node -e 'fetch(`http://localhost:3000/xrpc/_health`).then(()=>process.exitCode = 0).catch(()=>process.exitCode = 1)'"],
-      },
-      // PDS min system requirements: 1 CPU core, 1 GB memory, 20 GB disk
-      cpu: 1024,
-      memoryLimitMiB: 2048, // lowest mem value allowed in Fargate for 1 CPU
-      // Only let 1 PDS instance run at a time.
-      // Deployments will take down the old task before starting a new one
-      maxHealthyPercent: 100,
-      minHealthyPercent: 0,
-      circuitBreaker: {
-        enable: true,
-        rollback: true,
-      },
-      // Enable running pdsadmin in the container
-      enableExecuteCommand: true,
-    });
+        // PDS min system requirements: 1 CPU core, 1 GB memory, 20 GB disk
+        cpu: 1024,
+        memoryLimitMiB: 2048, // lowest mem value allowed in Fargate for 1 CPU
+        // Only let 1 PDS instance run at a time.
+        // Deployments will take down the old task before starting a new one
+        maxHealthyPercent: 100,
+        minHealthyPercent: 0,
+        circuitBreaker: {
+          enable: true,
+          rollback: true,
+        },
+        // Enable running pdsadmin in the container
+        enableExecuteCommand: true,
+      }
+    );
 
     service.targetGroup.configureHealthCheck({
       path: '/xrpc/_health',
     });
 
     // Add sidecar container that backs up and restores the PDS data to/from S3
-    const sidecar = service.taskDefinition.addContainer('SyncContainer',
-      {
-        containerName: 's3_sync',
-        image: ecs.ContainerImage.fromAsset(
-          './pds-data-backup',
-          {
-            platform: ecr_assets.Platform.LINUX_AMD64,
-          }
-        ),
-        logging: ecs.LogDriver.awsLogs({
-          streamPrefix: 'PDSS3Sync',
-          logGroup: new logs.LogGroup(this, 'PDSS3SyncLogGroup', {
-            retention: logs.RetentionDays.ONE_MONTH,
-            removalPolicy: RemovalPolicy.DESTROY,
-          }),
+    const sidecar = service.taskDefinition.addContainer('SyncContainer', {
+      containerName: 's3_sync',
+      image: ecs.ContainerImage.fromAsset('./pds-data-backup', {
+        platform: ecr_assets.Platform.LINUX_AMD64,
+      }),
+      logging: ecs.LogDriver.awsLogs({
+        streamPrefix: 'PDSS3Sync',
+        logGroup: new logs.LogGroup(this, 'PDSS3SyncLogGroup', {
+          retention: logs.RetentionDays.ONE_MONTH,
+          removalPolicy: RemovalPolicy.DESTROY,
         }),
-        environment: {
-          AWS_REGION: this.region,
-          AWS_DEFAULT_REGION: this.region,
-          S3_PATH: `s3://${dataBackupBucket.bucketName}/pds-backup`,
-          LOCAL_PATH: '/sync',
-        },
-        healthCheck: {
-          command: ['CMD', '/healthcheck.sh'],
-        },
-        stopTimeout: Duration.minutes(1),
-      }
-    );
+      }),
+      environment: {
+        AWS_REGION: this.region,
+        AWS_DEFAULT_REGION: this.region,
+        S3_PATH: `s3://${dataBackupBucket.bucketName}/pds-backup`,
+        LOCAL_PATH: '/sync',
+      },
+      healthCheck: {
+        command: ['CMD', '/healthcheck.sh'],
+      },
+      stopTimeout: Duration.minutes(1),
+    });
 
     // Create a volume for the PDS data
     service.taskDefinition.addVolume({
@@ -224,13 +224,17 @@ class BlueskyPdsInfraStack extends Stack {
       new iam.PolicyStatement({
         actions: ['ecr:BatchImportUpstreamImage'],
         resources: [cacheRepo.repositoryArn],
-      }),
+      })
     );
 
     // Permissions needed by containers
     dataBackupBucket.grantReadWrite(service.service.taskDefinition.taskRole);
     blobBucket.grantReadWrite(service.service.taskDefinition.taskRole);
-    rotationKey.grant(service.service.taskDefinition.taskRole, 'kms:GetPublicKey', 'kms:Sign');
+    rotationKey.grant(
+      service.service.taskDefinition.taskRole,
+      'kms:GetPublicKey',
+      'kms:Sign'
+    );
 
     // Alarms: monitor 500s and unhealthy hosts on target groups
     new cloudwatch.Alarm(this, 'TargetGroupUnhealthyHosts', {
@@ -259,7 +263,7 @@ new BlueskyPdsInfraStack(app, 'BlueskyPdsInfra', {
   rootDomain: 'clare.dev',
   env: { account: process.env['CDK_DEFAULT_ACCOUNT'], region: 'us-east-2' },
   tags: {
-      project: "bluesky-pds"
-  }
+    project: 'bluesky-pds',
+  },
 });
 app.synth();
